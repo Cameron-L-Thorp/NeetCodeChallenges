7d1a0c6b08481e87aee10ae21f90dcec69afcfd1
Cameron-L-Thorp <109598902+Cameron-L-Thorp@users.noreply.github.com>
2022-10-18 11:38:07 -0600

    Contains Duplicate Problem

---
+++ NeetCodeProblems/ContainsDuplicate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Channels;
+using System.Xml.Serialization;
+
+namespace ContainsDuplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] nums = { 1, 2, 3, 4 };
+            Solution.PrintSol(nums);
+        }
+    }
+
+    public static class Solution
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            List<int> distNums = new();
+            bool duplicate = false;
+            foreach (int i in nums)
+            {
+                if (distNums.Contains(i))
+                {
+                    duplicate = true;
+                }
+                distNums.Add(i);
+            }
+
+            return duplicate;
+        }
+
+        public static void PrintSol(int[] nums)
+        {
+            Console.WriteLine(Solution.ContainsDuplicate(nums));
+        }
+    }
+}
